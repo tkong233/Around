947548ec74d027489ece9dba17f1cd2246b84a2b
tkong233 <tkong@wesleyan.edu>
2018-10-28 19:11:21 -0400

    registration

--- around-web/src/App.js
+++ around-web/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import {TopBar} from './TopBar';
+import {Main} from "./Main"
 
 class App extends Component {
   render() {
     return (
         <div className="App">
           <TopBar/>
+            <Main/>
         </div>
     );
   }
 }
-
-
 
 
 export default App;
---
+++ around-web/src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Register} from './Registration';
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div className="main"><Register/></div>
+
+        );
+    }
+}
