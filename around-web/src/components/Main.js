3c032a744057703d58ee85c636c6f7d4e7ba7a1c
tkong233 <tkong@wesleyan.edu>
2018-10-30 23:23:44 -0400

    login UI completed

--- around-web/src/components/Main.js
+++ around-web/src/components/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Register} from './Registration';
+import {Login} from './Login';
 
 export class Main extends React.Component {
     render() {
         return (
-            <div className="main"><Register/></div>
+            <div className="main">
+                <Register/>
+                <Login/>
+            </div>
 
         );
     }
