f15cd9c06e8c56f9c2f1c60ffeb87b8e15bbeb92
tkong233 <tkong@wesleyan.edu>
2018-11-02 00:49:10 -0400

    Gallery

--- around-web/src/components/Home.js
+++ around-web/src/components/Home.js
@@ -1,64 +1,100 @@
 import React from 'react';
 import { Tabs, Button, Spin } from 'antd';
-import { GEO_OPTIONS } from '../constants';
+import { GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_HEADER, TOKEN_KEY } from '../constants';
+import { Gallery } from './Gallery';
 
 const TabPane = Tabs.TabPane;
-
-
 
 export class Home extends React.Component {
     state = {
         isLoadingGeoLocation: false,
         error: '',
+        isLoadingPosts: false,
+        posts: [],
     }
 
     componentDidMount() {
         if ("geolocation" in navigator) {
-            /* geolocation is available */
-            this.setState({ isLoadingGeoLocation: true, error: ''});
+            this.setState({ isLoadingGeoLocation: true, error: '' });
             navigator.geolocation.getCurrentPosition(
                 this.onSuccessLoadGeoLocation,
                 this.onFailedLoadGeoLocation,
-                GEO_OPTIONS
-            );
+                GEO_OPTIONS);
         } else {
-            this.setState({ isLoadingGeoLocation: false, error: 'Load geo location failed' })
-            /* geolocation IS NOT available */
+            this.setState({ error: 'Geolocation is not supported.'});
         }
     }
-
     onSuccessLoadGeoLocation = (position) => {
-        this.setState({ isLoadingGeoLocation: false, error: 'Load geo location failed' });
         console.log(position);
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({ lat: latitude, lon: longitude }));
+        this.setState({ isLoadingGeoLocation: false });
+        this.loadNearbyPosts();
     }
 
     onFailedLoadGeoLocation = () => {
-        this.setState({ isLoadingGeoLocation : false });
-        console.log('Failed to load geo location');
+        this.setState({ isLoadingGeoLocation: false, error: 'Failed to load geolocation.' });
     }
 
-    getImagePost = () => {
-        const{error, isLoadingGeoLocation} = this.state;
-        if(error) {
-            return <div>{error}</div>;
+    loadNearbyPosts = () => {
+        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+        const token = localStorage.getItem(TOKEN_KEY);
+        this.setState({ isLoadingPosts: true, error: '' });
+        fetch(`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20000`, {
+            method: 'GET',
+            headers: {
+                Authorization: `${AUTH_HEADER} ${token}`,
+            },
+        }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Failed to load posts.');
+        }).then((data) => {
+            console.log(data);
+            this.setState({ isLoadingPosts: false, posts: data ? data : [] });
+        }).catch((e) => {
+            console.log(e.message);
+            this.setState({ isLoadingPosts: false, error: e.message });
+        });
+    }
+
+    getImagePosts = () => {
+        const { error, isLoadingGeoLocation, isLoadingPosts, posts } = this.state;
+        if (error) {
+            return <div>{error}</div>
         } else if(isLoadingGeoLocation) {
-            return <Spin tip="is loading geo location..."/>;
+            return <Spin tip="Loading geo location..."/>
+        } else if (isLoadingPosts) {
+            return <Spin tip="Loading posts..." />
+        } else if (posts.length > 0) {
+            const images = this.state.posts.map((post) => {
+                return {
+                    user: post.user,
+                    src: post.url,
+                    thumbnail: post.url,
+                    caption: post.message,
+                    thumbnailWidth: 400,
+                    thumbnailHeight: 300,
+                }
+            });
+
+            return (<Gallery images={images}/>);
         } else {
-            return 'content of tab 1'
+            return 'No nearby posts.';
         }
-}
+    }
 
     render() {
         const operations = <Button type="primary">Create New Post</Button>;
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Image Posts" key="1">
-                    {this.getImagePost()}
+                    {this.getImagePosts()}
                 </TabPane>
                 <TabPane tab="Video Posts" key="2">Content of tab 2</TabPane>
                 <TabPane tab="Map" key="3">Content of tab 3</TabPane>
             </Tabs>
-
         );
     }
 }
--- around-web/src/constants.js
+++ around-web/src/constants.js
@@ -3,7 +3,7 @@ export const TOKEN_KEY = 'TOKEN_KEY';
 export const GEO_OPTIONS = {
     enableHighAccuracy: true,
     maximumAge        : 3600000,
-    timeout           : 27000
+    timeout           : 270000
 }
 export const POS_KEY = 'POS_KEY';
-export const AUTH_PREFIX = 'Bearer';
+export const AUTH_HEADER = 'Bearer';
